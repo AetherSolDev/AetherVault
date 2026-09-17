cc7593a3854c4bc4e8fe862846c251c194ab2c3c
AetherSol <support@aethersol.io>
2026-09-16 19:05:05 -0500

    Revert "chore: recover AetherVault sync relay source from the router image"
    This reverts commit cc082720370620e00beea6e666ebe3d6b669c460.

--- server/server/__init__.py
+++
@@ -1,6 +0,0 @@
-# Created: 2026-09-16
-# Last Edited: 2026-09-16 12:36 CT (America/Chicago)
-# Path: server/__init__.py
-# Purpose: Package marker for the AetherVault sync relay.
-
-"""AetherVault zero-knowledge sync relay (Phase 2)."""
